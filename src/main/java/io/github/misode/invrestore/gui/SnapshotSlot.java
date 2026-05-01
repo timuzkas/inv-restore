2ed96480f470b01a03de02654b168908be640367
timuzkas <timotiejus.lt@gmail.com>
2026-05-01 08:04:11 +0300

    Add invrestore.can_modify permission and update version to 0.1.8.1

--- src/main/java/io/github/misode/invrestore/gui/SnapshotSlot.java
+++ src/main/java/io/github/misode/invrestore/gui/SnapshotSlot.java
@@ -1,6 +1,9 @@
 package io.github.misode.invrestore.gui;
 
+import me.lucko.fabric.api.permissions.v0.Permissions;
+import net.minecraft.server.permissions.PermissionLevel;
 import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 
@@ -12,5 +15,10 @@ public class SnapshotSlot extends Slot {
     @Override
     public boolean mayPlace(ItemStack itemStack) {
         return false;
+    }
+
+    @Override
+    public boolean mayPickup(Player player) {
+        return Permissions.check(player, "invrestore.can_modify", PermissionLevel.GAMEMASTERS);
     }
 }
